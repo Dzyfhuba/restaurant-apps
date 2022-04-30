81ae46b610638b10535f37419fa26188a2f3166b
Dzyfhuba <hafidz21ub@gmail.com>
2022-04-30 16:43:09 +0700

    downgrade webpack and related module for submission purpose

--- webpack.common.js
+++ webpack.common.js
@@ -24,12 +24,12 @@ module.exports = {
             {
                 test: /\.s[ac]ss$/i,
                 use: [
-                  // Creates `style` nodes from JS strings
-                  "style-loader",
-                  // Translates CSS into CommonJS
-                  "css-loader",
-                  // Compiles Sass to CSS
-                  "sass-loader",
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
                 ],
             },
         ],
