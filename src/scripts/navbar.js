687b5234eb1a371934d331dc2eaec2b2ed52657d
Dzyfhuba <hafidz21ub@gmail.com>
2022-05-05 08:48:26 +0700

    update some feature

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,21 +1,28 @@
-var prevScrollpos = window.pageYOffset;
+let prevScrollpos = window.pageYOffset;
+const head = document.querySelector('#head');
 window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
+    let currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
         document.querySelector('nav').style.top = '0';
+        head.style.padding = '90px 0 0 0';
     } else {
         document.querySelector('nav').style.top = '-100%';
+        head.style.padding = '0';
     }
     prevScrollpos = currentScrollPos;
 };
 
-const toggle = document.querySelector('nav #nav-toggle input[type="checkbox"]');
+const toggle = document.querySelector('nav .nav-toggle');
 const nav = document.querySelector('nav');
+
+let indicator = false;
+
 // script below only for small screen
 if (window.innerWidth <= 767) {
     const ul = document.querySelector('nav ul');
-    toggle.addEventListener('change', (e) => {
-        if (e.target.checked) {
+    toggle.addEventListener('click', () => {
+        if (!indicator) {
+            indicator = !indicator;
             ul.style.display = 'flex';
             ul.animate([{
                 opacity: 0,
@@ -24,7 +31,7 @@ if (window.innerWidth <= 767) {
                 opacity: 1,
                 transform: 'translateY(100%)'
             }], {
-                duration: 500,
+                duration: 300,
                 fill: 'forwards'
             });
 
@@ -37,10 +44,12 @@ if (window.innerWidth <= 767) {
                     height: '100px',
                 }
             ], {
-                duration: 500,
+                duration: 300,
                 fill: 'forwards'
             });
         } else {
+            console.log(indicator);
+            indicator = !indicator;
             ul.animate([{
                 opacity: 1,
                 transform: 'translateY(100%)'
@@ -48,7 +57,7 @@ if (window.innerWidth <= 767) {
                 opacity: 0,
                 transform: 'translateY(0%)'
             }], {
-                duration: 500,
+                duration: 300,
                 fill: 'forwards'
             });
             // animate out
@@ -60,12 +69,12 @@ if (window.innerWidth <= 767) {
                     height: '50px',
                 },
             ], {
-                duration: 500,
+                duration: 300,
                 fill: 'forwards'
             });
             setTimeout(() => {
                 ul.style.display = 'none';
-            }, 500);
+            }, 300);
         }
     });
 }
